eee9af3eae6cd63481d3633512a97b4ecb03a042
Styopa Semenukha <ssemenukha@developmentgateway.org>
2019-05-01 11:24:05 -0400

    Write the rest of the owl

--- dh-renew.sh
+++ dh-renew.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 # Generate or renew DH params, and call a hook
 # Copyright 2019, Development Gateway, GPLv3+
+
+: ${DH_BITS:=2048}
+: ${NICENESS:=19}
+
+TMP="$(mktemp)"
+if nice -n "$NICENESS" openssl dhparam -out "$TMP" "$DH_BITS"; then
+  mv -f "$TMP" "$1"
+  shift
+  if [ -n "$@" ]; then
+    $@
+  fi
+fi
