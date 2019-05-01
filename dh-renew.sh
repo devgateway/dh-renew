e0cbba6855acf9cdcf6903c08e035e0f4281c0dc
Styopa Semenukha <ssemenukha@developmentgateway.org>
2019-05-01 12:00:26 -0400

    Convert spaces to tabs

--- dh-renew.sh
+++ dh-renew.sh
@@ -6,39 +6,39 @@
 : ${NICENESS:=19}
 
 if [ $# -lt 1 -o "$1" = '-h' -o "$1" = '--help' ]; then
-  cat >&2 <<EOF
+	cat >&2 <<EOF
 NAME
-  $(basename "$0") - generate or renew DH param, and call a hook.
+	$(basename "$0") - generate or renew DH param, and call a hook.
 
 SYNTAX
 
-  $(basename "$0") FILE [COMMAND] [ARG...]
+	$(basename "$0") FILE [COMMAND] [ARG...]
 
 OPTIONS
-  FILE
-    The DH param output.
+	FILE
+		The DH param output.
 
-  COMMAND, ARGS
-    Optional hook to call, e.g. to reload the application.
+	COMMAND, ARGS
+		Optional hook to call, e.g. to reload the application.
 
 ENVIRONMENT
-  DH_BITS[=$DH_BITS]
-    DH param size.
+	DH_BITS[=$DH_BITS]
+		DH param size.
 
-  NICENESS[=$NICENESS]
-    Add niceness to openssl(1ssl) process.
+	NICENESS[=$NICENESS]
+		Add niceness to openssl(1ssl) process.
 
 EXAMPLE
-  $(basename "$0") /etc/nginx/dh.param systemctl reload nginx
+	$(basename "$0") /etc/nginx/dh.param systemctl reload nginx
 EOF
-  exit
+	exit
 fi
 
 TMP="$(mktemp)"
 if nice -n "$NICENESS" openssl dhparam -out "$TMP" "$DH_BITS" 2>/dev/null; then
-  mv -f "$TMP" "$1"
-  shift
-  if [ -n "$@" ]; then
-    $@
-  fi
+	mv -f "$TMP" "$1"
+	shift
+	if [ -n "$@" ]; then
+		$@
+	fi
 fi
